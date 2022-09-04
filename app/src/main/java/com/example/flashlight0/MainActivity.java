9dfd37a1e86fa0d02693e31695ad6f453f32571e
tathagatamukherjee044 <tathagata.mukherjee044@gmail.com>
2022-09-04 18:13:29 +0530

    added blink with default 100o ms delay

--- app/src/main/java/com/example/flashlight0/MainActivity.java
+++ app/src/main/java/com/example/flashlight0/MainActivity.java
@@ -7,36 +7,57 @@ import android.content.Intent;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     boolean isSwitchOn= false;
     private boolean isBlink=false;
 
-    Button button;
+    Button flashButton, blinkButton;
     CameraManager cameraManager;
+    String cameraID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button=findViewById(R.id.button);
+        flashButton=findViewById(R.id.flashButton);
+        blinkButton = findViewById(R.id.blinkButton);
         startService(new Intent(MainActivity.this,ShakeDetection.class));
-
         cameraManager= (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            cameraID = cameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
 
         Intent i = new Intent(this,ShakeDetection.class);
         startService(i);
+
+        blinkButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                isBlink = !isBlink;
+                try {
+                    blinkOn();
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
     }
 
     public void toggle(View view) throws CameraAccessException {
-        String cameraID = null;
-        if (cameraManager != null)
-        {
-            cameraID=cameraManager.getCameraIdList()[0];
-        }
+
 
         if(isSwitchOn)
         {
@@ -48,5 +69,41 @@ public class MainActivity extends AppCompatActivity {
             cameraManager.setTorchMode(cameraID,true);
             isSwitchOn=true;
         }
+    }
+    
+    public void blinkOn() throws  CameraAccessException {
+
+        if(!isBlink) return;
+
+        Timer pulse = new Timer();
+        pulse.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    cameraManager.setTorchMode(cameraID,true);
+                    blinkOff();
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        },1000);
+    }
+
+    public void blinkOff() throws  CameraAccessException {
+
+        if(!isBlink) return;
+
+        Timer pulse = new Timer();
+        pulse.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    cameraManager.setTorchMode(cameraID,false);
+                    blinkOn();
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        },1000);
     }
 }
